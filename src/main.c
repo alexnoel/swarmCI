ac667ec18c1fdf7e9ba1ccb5b8b9e4ef7a73d767
Alexander Noel <anoel@g.clemson.edu>
2016-04-05 14:44:53 -0400

    Added Pwm test code and a basic main.c that uses it

---
+++ src/main.c
@@ -0,0 +1,59 @@
+#include <stdbool.h>
+#include <stdint.h>
+
+#include "inc/hw_types.h"
+#include "inc/hw_gpio.h"
+#include "inc/hw_memmap.h"
+#include "inc/hw_sysctl.h"
+#include "driverlib/gpio.h"
+#include "driverlib/rom.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/pin_map.h"
+#include "driverlib/can.h"
+
+
+#include "pwm_test.h"
+
+#define LED_RED GPIO_PIN_1
+#define LED_BLUE GPIO_PIN_2
+#define LED_GREEN GPIO_PIN_3
+
+int main()
+{
+    //setup the system clock
+	ROM_SysCtlClockSet(SYSCTL_SYSDIV_1 | SYSCTL_USE_OSC |   SYSCTL_OSC_MAIN | SYSCTL_XTAL_16MHZ);
+    
+	Init_PWM();
+
+	int count = 0;
+	int updown = 1;
+
+	start_PWM();
+		
+
+   for (;;) {
+	
+	
+	//set the pwm duty cycle for the red led, and increase to 100%, then come back to 0%.
+	//rinse and repeat
+	set_PWM_duty(count);
+
+	ROM_SysCtlDelay(45000);
+
+	if(count == 100){
+		updown = 0;
+	}
+	if(count == 0){
+		updown = 1;
+	}
+
+	if(updown == 1){
+		count++;
+	}
+	else{
+		count--;
+    }
+
+}	
+
+}
---
+++ src/pwm_test.c
@@ -0,0 +1,111 @@
+/***************************************************************************
+	PWM_test.c
+	
+	Test file for creating a pwm on a pin on one of the led pins
+***************************************************************************/
+
+#include "pwm_test.h"
+
+
+#include <stdbool.h>
+#include <stdint.h>
+
+#include "inc/hw_types.h"
+#include "inc/hw_gpio.h"
+#include "inc/hw_memmap.h"
+#include "inc/hw_sysctl.h"
+#include "driverlib/rom.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/pin_map.h"
+#include "driverlib/gpio.h"
+#include "driverlib/pwm.h"
+
+
+#define PWM_PERIOD_GEN_2 400
+
+//************************************************
+//
+// Initializes pwm on port PF1
+//
+//************************************************
+
+void Init_PWM(void){
+
+	//Configure PWM Clock to match system
+   	ROM_SysCtlPWMClockSet(SYSCTL_PWMDIV_1);
+
+	ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
+	ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_PWM1);
+
+	// module 1-pwm5 is on port pf1
+	//configure gpio for output
+	ROM_GPIOPinConfigure(GPIO_PF1_M1PWM5);
+	ROM_GPIOPinTypePWM(GPIO_PORTF_BASE, GPIO_PIN_1);
+
+	//configure pwm gen2 (gen2 covers pwm 4 and 5)
+	//gen_mode_down makes all pwm left aligned
+	//NO_SYNC deals with timer module synchronization, which we don't
+	//	particularly care about
+	//DBG_RUN allows the pwm module to continue to run when debug mode
+	//	causes execution to stop 
+	ROM_PWMGenConfigure(PWM1_BASE, PWM_GEN_2, PWM_GEN_MODE_DOWN | PWM_GEN_MODE_NO_SYNC | 		  PWM_GEN_MODE_DBG_RUN);
+
+
+	//set the period of the pwm module
+	ROM_PWMGenPeriodSet(PWM1_BASE,PWM_GEN_2, PWM_PERIOD_GEN_2);	
+	
+
+}
+
+//************************************************
+//
+// Sets the pwm duty cycle. 
+// input: duty - a 32 bit unsigned integer.
+// duty is clipped from 0 to 100 inclusive.
+//
+//************************************************
+
+void set_PWM_duty(int duty){
+
+	if(duty > 100) duty = 100;
+
+	if(duty < 0) duty = 0;
+
+	
+
+
+	ROM_PWMPulseWidthSet(PWM1_BASE, PWM_OUT_5, duty * 4);
+}
+
+
+//************************************************
+//
+// Starts a pwm running on the red LED at port PF1
+//
+//************************************************
+
+void start_PWM(void){
+
+	set_PWM_duty(50);
+	ROM_PWMGenEnable(PWM1_BASE, PWM_GEN_2);
+
+	ROM_PWMOutputState(PWM1_BASE, PWM_OUT_5_BIT, true);
+}
+
+
+
+//************************************************
+//
+// Stops the pwm running on the red led
+//
+//************************************************
+
+void stop_pwm(void){
+
+	ROM_PWMOutputState(PWM1_BASE, PWM_OUT_5_BIT, false);
+	ROM_PWMGenDisable(PWM1_BASE, PWM_GEN_2);
+}
+
+
+
+
