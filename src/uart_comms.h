af06ddf34bc281febb52d0a7757a355d38de684a
Joshua Dunster <jedunster@bellsouth.net>
2016-04-05 15:16:39 -0400

    Updated filenames and style of UART code

---
+++ src/uart_comms.h
@@ -0,0 +1,35 @@
+/************************************************************************
+	uart_comms.h
+	Header file for functions using a UART interface for serial
+	communication with a module connected to the TIVA.
+*************************************************************************/
+
+#ifndef UART_COMMS_H_
+#define UART_COMMS_H_
+
+//************************************************
+//
+// Initialize the given UART controller
+//
+//************************************************
+
+int initUART(uint32_t ui32Base);
+
+//************************************************
+//
+//  Write character out to the given UART controller
+//
+//************************************************
+
+int putCharUART(uint32_t ui32Base, char out);
+
+//************************************************
+//
+//  Read the current character from the given UART controller
+//
+//************************************************
+
+char getCharUART(uint32_t ui32Base);
+
+
+#endif /* UART_COMMS_H_ */
